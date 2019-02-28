07a925d3871baee00070b25f0c97c0a08ad774ef
Andrew Reid East <andrew@andreweast.net>
2019-02-28 20:23:47 +0000

    Shuffle list 100 times; choose best

--- hash.py
+++ hash.py
@@ -1,8 +1,10 @@
+from random import shuffle
+
 # filename = "a_example"
 # filename = "b_lovely_landscapes"
 # filename = "c_memorable_moments"
-# filename = "d_pet_pictures"
-filename = "e_shiny_selfies"
+filename = "d_pet_pictures"
+# filename = "e_shiny_selfies"
 infile = "%s.txt" % filename
 outfile = "%s_output.txt" % filename
 
@@ -28,52 +30,84 @@ for item in range(num_photos):
 file.close()
 # print(photos)
 
-# Make slideshow
-slideshow = []
+results_output = []
+results_score = []
+for shuffled in range(100):
 
-# for p in photos:
-#     slideshow.append([p])
+    # Make slideshow
+    slideshow = []
 
-for p in h_photos:
-    slideshow.append([p])
+    # for p in photos:
+    #     slideshow.append([p])
 
-for i in range(0, len(v_photos), 2):
-    # print(i)
-    slideshow.append([v_photos[i], v_photos[i+1]])
+    for p in h_photos:
+        slideshow.append([p])
 
-
-
-# Calc score
-def get_tags(photos):
-    tags = set()
-    for photo in photos:
-        for tag in photo['tags']:
-            tags.add(tag)
-    # print(tags)
-    return tags
-
-print("scores")
-score = 0
-for i in range(len(slideshow) - 1):
-    tagsSlideA = get_tags(slideshow[i])
-    tagsSlideB = get_tags(slideshow[i + 1])
-    commonTags = tagsSlideA.intersection(tagsSlideB)
-    # print("Tags slide A: ", tagsSlideA)
-    # print("Tags slide B: ", tagsSlideB)
-    # print("common: ", commonTags)
-    # print("score: " + str(min(len(tagsSlideA), len(tagsSlideB), len(commonTags))))
-    score += min(len(tagsSlideA), len(tagsSlideB), len(commonTags))
-print("score: ", score)
+    for i in range(0, len(v_photos), 2):
+        # print(i)
+        slideshow.append([v_photos[i], v_photos[i+1]])
 
 
+    # Stoichiastic method
+    if shuffled != 0:
+        shuffle(slideshow)
+
+    # Calc score
+    def get_tags(photos):
+        tags = set()
+        for photo in photos:
+            for tag in photo['tags']:
+                tags.add(tag)
+        # print(tags)
+        return tags
+
+    print("scores")
+    score = 0
+    for i in range(len(slideshow) - 1):
+        tagsSlideA = get_tags(slideshow[i])
+        tagsSlideB = get_tags(slideshow[i + 1])
+        commonTags = tagsSlideA.intersection(tagsSlideB)
+        # print("Tags slide A: ", tagsSlideA)
+        # print("Tags slide B: ", tagsSlideB)
+        # print("common: ", commonTags)
+        # print("score: " + str(min(len(tagsSlideA), len(tagsSlideB), len(commonTags))))
+        score += min(len(tagsSlideA), len(tagsSlideB), len(commonTags))
+    print("score: ", score)
 
 
 
-# Output slideshow
+    # Cache results
+    results_score.append(score)
+
+    result = ""
+    result += str(len(slideshow)) + "\n"
+    for photo in slideshow:
+        for item in photo:
+            result += str(item['i']) + " "
+        result += "\n"
+    results_output.append(result)
+
+
+# # Output slideshow
+# file = open(outfile, 'w')
+# file.write(str(len(slideshow)) + "\n")
+# for photo in slideshow:
+#     for item in photo:
+#         file.write(str(item['i']) + " ")
+#     file.write("\n")
+# file.close()
+
+print(results_score)
+
+least = results_score[0]
+least_i = 0
+for i in range(1, len(results_score)):
+    if results_score[i] > least:
+        least_i = i
+        least = results_score[i]
+
+print("writing one with score: ", least)
 file = open(outfile, 'w')
-file.write(str(len(slideshow)) + "\n")
-for photo in slideshow:
-    for item in photo:
-        file.write(str(item['i']) + " ")
-    file.write("\n")
+file.write(results_output[least_i])
 file.close()
+
