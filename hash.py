5e0033a4bce9759d6513373aee5f62102f27ea9b
Andrew Reid East <andrew@andreweast.net>
2019-02-28 19:27:56 +0000

    Working by brute force serach N^2

--- hash.py
+++ hash.py
@@ -1,8 +1,8 @@
 # filename = "a_example"
-# filename = "b_lovely_landscapes"
+filename = "b_lovely_landscapes"
 # filename = "c_memorable_moments"
 # filename = "d_pet_pictures"
-filename = "e_shiny_selfies"
+# filename = "e_shiny_selfies"
 infile = "%s.txt" % filename
 outfile = "%s_output.txt" % filename
 
@@ -28,22 +28,14 @@ for item in range(num_photos):
 file.close()
 # print(photos)
 
-# Make slideshow
-slideshow = []
-
-# for p in photos:
-#     slideshow.append([p])
-
+slides = []
+# slides.extend(h_photos)
 for p in h_photos:
-    slideshow.append([p])
-
+    slides.append([p])
 for i in range(0, len(v_photos), 2):
     # print(i)
-    slideshow.append([v_photos[i], v_photos[i+1]])
+    slides.append([v_photos[i], v_photos[i+1]])
 
-
-
-# Calc score
 def get_tags(photos):
     tags = set()
     for photo in photos:
@@ -52,17 +44,63 @@ def get_tags(photos):
     # print(tags)
     return tags
 
-print("scores")
-score = 0
-for i in range(len(slideshow) - 1):
-    tagsSlideA = get_tags(slideshow[i])
-    tagsSlideB = get_tags(slideshow[i + 1])
+def get_score(slideA, slideB):
+    tagsSlideA = get_tags(slideA)
+    tagsSlideB = get_tags(slideB)
     commonTags = tagsSlideA.intersection(tagsSlideB)
     # print("Tags slide A: ", tagsSlideA)
     # print("Tags slide B: ", tagsSlideB)
     # print("common: ", commonTags)
     # print("score: " + str(min(len(tagsSlideA), len(tagsSlideB), len(commonTags))))
-    score += min(len(tagsSlideA), len(tagsSlideB), len(commonTags))
+    return min(len(tagsSlideA), len(tagsSlideB), len(commonTags))
+
+# Make slideshow
+slideshow = []
+
+# for p in photos:
+#     slideshow.append([p])
+
+# for p in h_photos:
+#     slideshow.append([p])
+#
+# for i in range(0, len(v_photos), 2):
+#     # print(i)
+#     slideshow.append([v_photos[i], v_photos[i+1]])
+
+# Always make the first potential slide the first in slideshow
+slideshow.append(slides[0])
+del slides[0]
+
+# Add each of the remaining slides (in some order)
+total_potential_slides = len(slides)
+for i in range(total_potential_slides):
+    firstSlide = slideshow[i]
+    lowest_score = 100000000000000
+    lowest_item = -1
+    for j in range(len(slides)):
+        score = get_score(firstSlide, slides[j])
+        if score != 0:
+            if score < lowest_score:
+                # print("found lower", j, score, slides[j])
+                lowest_score = score
+                lowest_item = j
+            # else:
+            #     print("not lower:", j)
+    # print('lowest: ', lowest_item)
+    print(str(i) + ",", end="")
+
+    slideshow.append(slides[lowest_item])
+    del slides[lowest_item]
+
+
+
+
+# Calc score
+print("scores")
+print("length slideshow", len(slideshow))
+score = 0
+for i in range(len(slideshow) - 1):
+    score += get_score(slideshow[i], slideshow[i + 1])
 print("score: ", score)
 
 
