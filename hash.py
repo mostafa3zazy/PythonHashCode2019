9bc256ce08a53f300daee16c38b966020e10c8c1
Andrew Reid East <andrew@andreweast.net>
2019-02-28 19:42:38 +0000

    Optmise by only making set of tags for V photos once

--- hash.py
+++ hash.py
@@ -1,10 +1,31 @@
-# filename = "a_example"
-filename = "b_lovely_landscapes"
+from random import randint
+
+filename = "a_example"
+# filename = "b_lovely_landscapes"
 # filename = "c_memorable_moments"
 # filename = "d_pet_pictures"
 # filename = "e_shiny_selfies"
 infile = "%s.txt" % filename
 outfile = "%s_output.txt" % filename
+
+def get_tags(photos):
+    tags = set()
+    for photo in photos:
+        for tag in photo['tags']:
+            tags.add(tag)
+    # print(tags)
+    return tags
+
+def get_score(slideA, slideB):
+    # tagsSlideA = get_tags(slideA)
+    # tagsSlideB = get_tags(slideB)
+    commonTags = slideA['tags'].intersection(slideB['tags'])
+    # print("Tags slide A: ", tagsSlideA)
+    # print("Tags slide B: ", tagsSlideB)
+    # print("common: ", commonTags)
+    # print("score: " + str(min(len(tagsSlideA), len(tagsSlideB), len(commonTags))))
+    return min(len(slideA['tags']), len(slideB['tags']), len(commonTags))
+
 
 # Read file
 file = open(infile, 'r')
@@ -15,7 +36,7 @@ v_photos = []
 for item in range(num_photos):
     line = file.readline().split()
     p = {}
-    p['i'] = item
+    p['i'] = str(item)
     p['orient'] = line[0]
     p['tags'] = []
     for tag in line[2:]:
@@ -31,28 +52,10 @@ file.close()
 slides = []
 # slides.extend(h_photos)
 for p in h_photos:
-    slides.append([p])
+    slides.append({'i': p['i'], 'tags': set(p['tags'])})
 for i in range(0, len(v_photos), 2):
-    # print(i)
-    slides.append([v_photos[i], v_photos[i+1]])
-
-def get_tags(photos):
-    tags = set()
-    for photo in photos:
-        for tag in photo['tags']:
-            tags.add(tag)
-    # print(tags)
-    return tags
-
-def get_score(slideA, slideB):
-    tagsSlideA = get_tags(slideA)
-    tagsSlideB = get_tags(slideB)
-    commonTags = tagsSlideA.intersection(tagsSlideB)
-    # print("Tags slide A: ", tagsSlideA)
-    # print("Tags slide B: ", tagsSlideB)
-    # print("common: ", commonTags)
-    # print("score: " + str(min(len(tagsSlideA), len(tagsSlideB), len(commonTags))))
-    return min(len(tagsSlideA), len(tagsSlideB), len(commonTags))
+    # slides.append([v_photos[i], v_photos[i+1]])
+    slides.append({'i': v_photos[i]['i'] + " " + v_photos[i+1]['i'], 'tags': get_tags([v_photos[i], v_photos[i + 1]])})
 
 # Make slideshow
 slideshow = []
@@ -78,6 +81,8 @@ for i in range(total_potential_slides):
     lowest_score = 100000000000000
     lowest_item = -1
     for j in range(len(slides)):
+    # for k in range(10):
+    #     j = randint(0, len(slides) - 1)
         score = get_score(firstSlide, slides[j])
         if score != 0:
             if score < lowest_score:
@@ -87,7 +92,8 @@ for i in range(total_potential_slides):
             # else:
             #     print("not lower:", j)
     # print('lowest: ', lowest_item)
-    print(str(i) + ",", end="")
+    if i % 100 == 0:
+        print(str(i) + ",", end="")
 
     slideshow.append(slides[lowest_item])
     del slides[lowest_item]
@@ -110,8 +116,8 @@ print("score: ", score)
 # Output slideshow
 file = open(outfile, 'w')
 file.write(str(len(slideshow)) + "\n")
-for photo in slideshow:
-    for item in photo:
-        file.write(str(item['i']) + " ")
+for slide in slideshow:
+    # for item in slide:
+    file.write(str(slide['i']) + " ")
     file.write("\n")
 file.close()
